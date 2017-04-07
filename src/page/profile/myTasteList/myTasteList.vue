66501989220cb8307a6b81533fc2a3c2690f6dc8
陈海俊 <www.353376352@qq.com>
2017-04-08 00:32:54 +0800

    home at 20170408 0032

--- src/page/profile/myTasteList/myTasteList.vue
+++ src/page/profile/myTasteList/myTasteList.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
-    myTasteList
-  </div>
+  <section class="myTasteList" v-if="userInfo">
+    {{ userInfo.myTasteList.length }}
+  </section>
 </template>
 
 <script type="text/ecmascript-6">
   export default{
-    name: 'myTasteList'
+    name: 'myTasteList',
+    props: {
+      userInfo: {
+        type: Object
+      }
+    }
   }
 </script>
 
