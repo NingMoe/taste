340cc1fed122f7a980e895d9d953a5ec1f297995
陈海俊 <www.353376352@qq.com>
2017-04-18 01:23:41 +0800

    home 0418 0123

--- src/page/other/join/join.vue
+++ src/page/other/join/join.vue
@@ -17,19 +17,31 @@
         <ul>
           <li class="form-line">
             <label>企业名称: </label>
-            <div class="input-container"><input type="text" class="input"></div>
+            <div class="input-container">
+              <input v-validate="'required'" name="name" type="text" class="input">
+              <span v-show="errors.has('name')" class="is-danger">* 必填</span>
+            </div>
           </li>
          <li class="form-line">
             <label>合作事项: </label>
-            <div class="input-container"><input type="text" class="input"></div>
+            <div class="input-container">
+              <input type="text" v-validate="'required'" class="input" name="business">
+              <span v-show="errors.has('business')" class="is-danger">* 必填</span>
+            </div>
           </li>
          <li class="form-line">
             <label>预计费用: </label>
-            <div class="input-container"><input type="text" class="input"></div>
+            <div class="input-container">
+              <input type="text" v-validate="'required'" class="input" name="cost">
+              <span v-show="errors.has('cost')" class="is-danger">* 必填</span>
+            </div>
           </li>
          <li class="form-line">
             <label>联系电话: </label>
-            <div class="input-container"><input type="text" class="input"></div>
+            <div class="input-container">
+              <input type="text" v-validate="'required'" class="input" name="phone">
+              <span v-show="errors.has('phone')" class="is-danger">* 必填</span>
+            </div>
           </li>
         </ul>
 
@@ -37,7 +49,6 @@
           <div class="pic-form-line">
             <div>上传营业执照：</div>
             <div class="pics-container">
-
               <div class="pics-item common-addPic-btn"></div>
             </div>
           </div>
@@ -53,18 +64,6 @@
           <div class="pic-form-line">
             <div>上传产品图片（最多9张）：</div>
             <div class="pics-container">
-              <div class="pics-item">
-                <img src="/static/temp/item-face.jpg" alt="">
-              </div>
-              <div class="pics-item">
-                <img src="/static/temp/item-face.jpg" alt="">
-              </div>
-              <div class="pics-item">
-                <img src="/static/temp/item-face.jpg" alt="">
-              </div>
-              <div class="pics-item">
-                <img src="/static/temp/item-face.jpg" alt="">
-              </div>
               <div class="pics-item common-addPic-btn"></div>
             </div>
           </div>
@@ -102,14 +101,20 @@
       .form-line
         display: flex
         align-items: center
-        margin: 12px 0
+        margin: 12px 0 18px 0
       .input-container
         flex: 1
+        position: relative
         padding-left: 10px
         .input
           padding: 5px
           width: 100%
           border: 1px solid #fabe00
+        .is-danger
+          position: absolute
+          bottom: -14px
+          color: red
+          font-size: 12px
       .pic-form-container
         margin-top: 30px
         .pics-container
